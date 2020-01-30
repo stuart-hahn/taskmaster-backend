531c87a52aac7a3f87b56823cd25e1c099a37c57
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-01-30 11:12:19 -0500

    create controller actions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,52 @@
-class ProjectsController < ApplicationController
+class Api::V1::ProjectsController < ApplicationController
+    before_action :set_project, only: [:show, :update, :destroy]
+
+    # GET /projects
+    def index
+      @projects = Project.all
+  
+      render json: ProjectSerializer.new(@projects)
+    end
+  
+    # GET /projects/1
+    def show
+      render json: ProjectSerializer.new(@project)
+    end
+  
+    # POST /projects
+    def create
+      @project = Project.new(project_params)
+  
+      if @project.save
+        render json: ProjectSerializer(@project), status: :created, location: @project
+      else
+        render json: @project.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /projects/1
+    def update
+      if @project.update(project_params)
+        render json: ProjectSerializer.new(@project)
+      else
+        render json: @project.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /projects/1
+    def destroy
+      @project.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_project
+        @project = Project.find_by(id: params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def project_params
+        params.require(:project).permit(:title)
+      end
 end
+
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,52 @@
-class TasksController < ApplicationController
+class Api::V1::TasksController < ApplicationController
+    before_action :set_task, only: [:show, :update, :destroy]
+
+    # GET /tasks
+    def index
+      @tasks = Task.all
+  
+      render json: TaskSerializer.new(@tasks)
+    end
+  
+    # GET /tasks/1
+    def show
+      render json: TaskSerializer.new(@task)
+    end
+  
+    # POST /tasks
+    def create
+      @task = Task.new(task_params)
+  
+      if @task.save
+        render json: TaskSerializer(@task), status: :created, location: @task
+      else
+        render json: @task.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /tasks/1
+    def update
+      if @task.update(task_params)
+        render json: TaskSerializer.new(@task)
+      else
+        render json: @task.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /tasks/1
+    def destroy
+      @task.destroy
+    end
+  
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_task
+        @task = Task.find_by(id: params[:id])
+      end
+  
+      # Only allow a trusted parameter "white list" through.
+      def task_params
+        params.require(:task).permit(:title, :project)
+      end
 end
+
